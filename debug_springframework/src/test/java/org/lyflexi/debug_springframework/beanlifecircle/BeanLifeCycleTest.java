24fca95de7b70e43d2dde18e7fa816da8f235b14
ly <liuyanoutsee@outlook.com>
2024-01-01 22:02:08 +0800

    delete ./idea

--- debug_springframework/src/test/java/org/lyflexi/debug_springframework/beanlifecircle/BeanLifeCycleTest.java
+++ debug_springframework/src/test/java/org/lyflexi/debug_springframework/beanlifecircle/BeanLifeCycleTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 
 public class BeanLifeCycleTest {
+    //
 
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:beanlifecircle/applicationContext.xml");
