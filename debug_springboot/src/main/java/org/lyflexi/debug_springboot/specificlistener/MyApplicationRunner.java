498ed94214cb531035b5eadcfd1687372e749bee
ly <liuyanoutsee@outlook.com>
2024-01-31 11:58:38 +0800

    springboot3-可观测性actuator,MODIFY

--- debug_springboot/src/main/java/org/lyflexi/debug_springboot/specificlistener/MyApplicationRunner.java
+++ debug_springboot/src/main/java/org/lyflexi/debug_springboot/specificlistener/MyApplicationRunner.java
@@ -4,6 +4,9 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @Author: ly
  * @Date: 2024/1/29 21:10
@@ -13,5 +16,9 @@ public class MyApplicationRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("MyApplicationRunner...run...");
+        System.out.println("MyApplicationRunner Arrays.asList(args):"+Arrays.asList(args));
+        System.out.println("MyApplicationRunner args.getOptionNames():"+args.getOptionNames());
+        System.out.println("MyApplicationRunner args.getOptionValues(\"name\"):"+args.getOptionValues("name"));
+        System.out.println("MyApplicationRunner args.getOptionValues(\"email\"):"+args.getOptionValues("email"));
     }
 }
--- debug_springboot/src/main/java/org/lyflexi/debug_springboot/specificlistener/MyCommandLineRunner.java
+++ debug_springboot/src/main/java/org/lyflexi/debug_springboot/specificlistener/MyCommandLineRunner.java
@@ -3,6 +3,8 @@ package org.lyflexi.debug_springboot.specificlistener;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 /**
  * @Author: ly
  * @Date: 2024/1/29 21:12
@@ -12,5 +14,6 @@ public class MyCommandLineRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("MyCommandLineRunner....run....");
+        System.out.println("MyCommandLineRunner Arrays.asList(args):" + Arrays.asList(args));
     }
 }
